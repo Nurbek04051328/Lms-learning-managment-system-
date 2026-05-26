53fae3be7a7ce8525e14b9410b619a1a3475bff1
Nurbek04051328 <bek050613@mail.ru>
2026-05-26 23:03:02 +0500

    feat: auth SingUpPage added

--- front/src/modules/auth/pages/SignUpPage.vue
+++ front/src/modules/auth/pages/SignUpPage.vue
@@ -5,12 +5,20 @@
       <div
         class="md:w-[50%] w-[100%] h-[100%] flex flex-col items-center justify-center gap-3"
       >
+        <div>
+          <h1 class="font-semibold text-black text-2xl">let's get started</h1>
+          <h2 class="text-[#999797] text-[18px]">Create your account</h2>
+        </div>
+        <div class="flex flex-col gap-1 w-[80%] items-start justify-center px-3">
+          <label for=""></label>
+        </div>
       </div>
       <!-- right -->
       <div
         class="w-[50%] h-[100%] rounded-r-2xl bg-[black] md:flex flex-col items-center justify-center hidden"
       >
-        <img src="../../../assets/logo.jpg" alt="Logo">
+        <img src="../../../assets/logo.jpg" alt="Logo" class="w-30 shadow-2xl" />
+        <span class="text-white text-2xl">VIRTUAL COURSES</span>
       </div>
     </form>
   </div>
